4b5b18af53e99503953a57a84357df6bf400b822
Victoria Stasenko <victoria.stasenko@smeetz.com>
2023-10-03 12:54:37 +0300

    main_added routing

---
+++ components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Nuxt SSR</a>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <nuxt-link exact no-prefetch active-class="active" class="nav-link" to="/">Home</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link active-class="active" class="nav-link" to="/about">About</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link active-class="active" class="nav-link" to="/users">Users</nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+  <section>
+    <h1>About this project</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati vitae exercitationem perspiciatis animi saepe distinctio doloremque quod libero repellat optio praesentium minima, deleniti expedita quibusdam sit! Voluptate exercitationem eligendi velit.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, qui tempore illo, velit exercitationem veritatis laboriosam beatae temporibus quidem facere error voluptatum. Et, eligendi facilis accusamus quas maxime vero quod.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum sed aliquid dolor, perspiciatis architecto dicta voluptates facilis doloribus vero veniam vitae aut commodi omnis accusantium deserunt eum voluptatem. Illum, tempora.</p>
+  </section>
+</template>
